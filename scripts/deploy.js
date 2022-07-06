931edcca63ee7a88c61bf02aec492317bb9e6a02
Jeffery Oke-Samuel <jefferyokesamuel1@gmail.com>
2022-07-06 13:20:44 +0100

    Delete scripts directory

--- scripts/deploy.js
+++
@@ -1,6 +0,0 @@
-const { hexStripZeros } = require("ethers/lib/utils")
-
-const main = async () => {
-    const [deployer] = await hexStripZeros.ethers.getSigners();
-    const accountBalance = await deployer.balance()
-}
