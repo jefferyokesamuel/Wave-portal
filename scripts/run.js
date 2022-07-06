77789f1965b509e46ef515d5faedf577a0d9a41c
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-06 19:08:55 +0100

    wave portal

---
+++ scripts/run.js
@@ -0,0 +1,29 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners()
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy;
+    await waveContract.deployed;
+
+    console.log('Contract deployed to: ', waveContract.address);
+    console.log('Contract deployed by: ', owner.address);
+
+    // let waveTest
+    // waveTest = await waveContract.getTotalWaves()
+    
+    let firstWave = await waveContract.wave()
+    await firstWave.wait()
+
+    waveTest = await waveContract.getTotalWaves()
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+runMain();
