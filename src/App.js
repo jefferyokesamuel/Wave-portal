0b0e09f441d8c1327ae1652e096360b28e6eeb10
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-07 16:08:39 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,31 +2,51 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-   
+  const checkIfWalletIsConnected = async () => {
+    //First make sure we have access to window.ethereum
+   try {
     const { ethereum } = window;
 
     if (!ethereum) {
       console.log("Make sure you have metamask!");
+      return;
     } else {
       console.log("We have the ethereum object", ethereum);
     }
-  }
-  // CHeck if we can access the users Wallet
-  const accounts = await ethereum.request({ method: "eth_accounts" });
+  // Check if we can access the users Wallet
 
-  if(accounts.length !== 0) {
-    const account = accounts[0]
-    console.log("Found an authorised account", account)
-    setCurrentAccount(account)
-  }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
+    if(accounts.length !== 0) {
+      const account = accounts[0]
+     console.log("Found an authorised account", account)
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    } 
+    }catch (error) {
+    console.log(error);
+    }
+  }
+  //Connecting to the Wallet
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask")
+        return
+      }
+    
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"})
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
   //This runs our function when the page loads.
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -49,6 +69,7 @@ const App = () => {
       </div>
     </div>
   );
+
 }
 
 export default App
