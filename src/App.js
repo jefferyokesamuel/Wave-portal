f20a588b617db41fb674aac15ac8e116432efbea
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-08 20:14:44 +0100

    changing

--- src/App.js
+++ src/App.js
@@ -5,49 +5,51 @@ const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
-    //First make sure we have access to window.ethereum
-   try {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
-  // Check if we can access the users Wallet
-
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if(accounts.length !== 0) {
-      const account = accounts[0]
-     console.log("Found an authorised account", account)
-      setCurrentAccount(account)
-    } else {
-      console.log("No authorized account found")
-    } 
-    }catch (error) {
-    console.log(error);
-    }
-  }
-  //Connecting to the Wallet
-  const connectWallet = async () => {
     try {
-      const {ethereum} = window;
+      const { ethereum } = window;
 
       if (!ethereum) {
-        alert("Get MetaMask")
-        return
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
       }
-    
-      const accounts = await ethereum.request({ method: "eth_requestAccounts"})
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
     }
   }
-  //This runs our function when the page loads.
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -56,20 +58,28 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        Welcome to the Wave Portal👨🏾‍💻👋 
+        Yoo Welcome to the Wave Portal 👨🏾‍💻👋
         </div>
 
         <div className="bio">
-          Hey im Jeffery a Blockchain Developer, I love building Dope projects, You can connect your Ethereum wallet and wave at me!
+          Hey, Im Jeffery a Blockchain Developer, Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
-
 }
 
 export default App
