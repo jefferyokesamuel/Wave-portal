a154620a2db02c4cfcdb82c1870490a3f8b55b66
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-13 00:26:21 +0100

    changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,9 @@ const main = async () => {
     console.log("Account balance: ", accountBalance.toString());
    
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+      value: hre
+    });
     await waveContract.deployed();
    
     console.log("WavePortal address: ", waveContract.address);
